03963f4e6869877de98a988aa4bb7487d9b7b304
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-27 23:40:22 -0400

    the method's body is started

--- WBE_IntegerToRoman/Program.cs
+++ WBE_IntegerToRoman/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Write a function that accepts an integer, and returns that integer in Roman numerals (as a string).
+ * Assume the number will be 0 < n < 40
+ * ie: 4 should return "IV"
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,37 @@ namespace WBE_IntegerToRoman
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("Enter an integer\n\n>>> ");
+                    int input = int.Parse(Console.ReadLine().Trim());
+                    Console.WriteLine(IntToRomanNumeral(input));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message);
+                }
+                Console.Write("\nPress Enter to try another string...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static string IntToRomanNumeral(int input)
+        {
+            throw new NotImplementedException();
+            //string roman = "";
+            //int deci = 0;
+            //while (input >= 10)
+            //{
+            //    input %= 10;
+            //    deci++;
+            //}
+            //input -= deci * 10;
+
+            //return roman;
         }
     }
 }
